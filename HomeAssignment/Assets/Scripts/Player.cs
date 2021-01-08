62e4060bdfd6d6216d0228376bd2d96e47465047
GarXik <GarXik@users.noreply.github.com>
2021-01-08 12:17:42 +0100

    Commit 2 - Background Scroller, Car Movement, Boundaries
    Task 1 - e,f,g
    8/1/2021 - 12:17PM

---
+++ HomeAssignment/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.7f;
+    float xMin, xMax;
+
+    //Camera Border/Boundary
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+       
+        var newXpos = transform.position.x + deltaX;
+
+        newXpos = Mathf.Clamp(newXpos, xMin, xMax);//creates a boundary for x axis
+
+        //move the player car to the new xpos
+        this.transform.position = new Vector2(newXpos, transform.position.y);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
